fd87295af94a12329e4605d4b274002e6f1fdf0c
Рязанцев Александр <alex.ryazantsev.80@mail.ru>
2025-09-19 01:42:04 +0700

    Имя студента

--- app.py
+++ app.py
@@ -412,4 +412,5 @@ def add_flower(name):
 
 @app.route('/lab2/example')
 def example():
-    return render_template('example.html')
+    name = 'Рязанцев Александр Алексеевич'
+    return render_template('example.html', name=name)
