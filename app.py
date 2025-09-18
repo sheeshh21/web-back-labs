b0f33615f13fa5d50024c9f51901020418a21c38
Рязанцев Александр <alex.ryazantsev.80@mail.ru>
2025-09-19 02:16:38 +0700

    Цикл в шаблоне

--- app.py
+++ app.py
@@ -416,4 +416,11 @@ def example():
     number_lab = '2'
     group = 'ФБИ-31'
     course = '3 курс'
-    return render_template('example.html', number_lab=number_lab, name=name, group=group, course=course)
+    fruits = [
+        {'name': 'яблоки', 'price': 100},
+        {'name': 'груши', 'price': 120},    
+        {'name': 'апельсины', 'price': 80},    
+        {'name': 'мандарины', 'price': 95},    
+        {'name': 'манго', 'price': 321}        
+    ]
+    return render_template('example.html', number_lab=number_lab, name=name, group=group, course=course, fruits=fruits)
