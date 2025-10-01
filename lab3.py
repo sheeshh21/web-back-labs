1c7b8a29ff85d918ab92eb384358891a6cc4143f
Рязанцев Александр <alex.ryazantsev.80@mail.ru>
2025-10-01 18:37:51 +0700

    валидация

--- lab3.py
+++ lab3.py
@@ -28,10 +28,18 @@ def del_cookie():
 
 @lab3.route('/lab3/form1')
 def form1():
+    errors = {}
     user = request.args.get('user')
+    if user == '':
+        errors['user'] = 'Заполните поле!'
+
+    errors1 = {}
     age = request.args.get('age')
+    if age == '':
+        errors1['age'] = 'Вы забыли указать возраст!'
+
     sex = request.args.get('sex')
-    return render_template('lab3/form1.html', user=user, age=age, sex=sex)
+    return render_template('lab3/form1.html', user=user, age=age, sex=sex, errors=errors, errors1=errors1)
 
 
 
