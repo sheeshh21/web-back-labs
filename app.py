8a507c3f586a4f95b82edc3bf9fd95f1dc1cc053
Рязанцев Александр <alex.ryazantsev.80@mail.ru>
2025-10-01 16:50:56 +0700

    заготовка к третьей лабораторной работе

--- app.py
+++ app.py
@@ -2,10 +2,12 @@ from flask import Flask, url_for, request, redirect, abort, render_template
 import datetime
 from lab1 import lab1
 from lab2 import lab2
+from lab3 import lab3
 
 app = Flask(__name__)
 app.register_blueprint(lab1)
 app.register_blueprint(lab2)
+app.register_blueprint(lab3)
 
 @app.route("/index")
 def index():
@@ -36,6 +38,7 @@ def a():
                 <header>НГТУ, ФБ, WEB-программирование, часть 2. Список лабораторных</header>
                     <div class='spisoklab'><a href="/lab1">Лабораторная работа №1</a></div>
                     <div class='spisoklab'><a href="/lab2">Лабораторная работа №2</a></div>
+                    <div class='spisoklab'><a href="/lab3">Лабораторная работа №3</a></div>
                 <footer>Рязанцев Александр Алексеевич, ФБИ-31, 3 курс, 2025</footer>
           </body>
         </html>'''
