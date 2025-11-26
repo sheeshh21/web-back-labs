5b5690af20742fd36af6f17efd4c3eb69984db3f
Рязанцев Александр <alex.ryazantsev.80@mail.ru>
2025-11-26 17:07:21 +0700

    Редактирование выбранного фильма по REST

--- lab7.py
+++ lab7.py
@@ -66,6 +66,16 @@ def get_film(id):
 def del_film(id):
     if id < 0 or id >= len(films):
         abort(404)
-        
+
     del films[id]
     return '', 204
+
+
+@lab7.route('/lab7/rest-api/films/<int:id>', methods=['PUT'])
+def put_film(id):
+    if id < 0 or id >= len(films):
+        abort(404)
+        
+    film = request.get_json()
+    films[id] = film
+    return films[id]
